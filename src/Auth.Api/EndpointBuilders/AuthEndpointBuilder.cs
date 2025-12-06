812c92092cb2cfd32675518b0638b781b524bfd4
Gramli <Gramli@seznam.cz>
2025-12-06 20:48:17 +0100

    -dotnet10, - CPM

--- src/Auth.Api/EndpointBuilders/AuthEndpointBuilder.cs
+++ src/Auth.Api/EndpointBuilders/AuthEndpointBuilder.cs
@@ -25,23 +25,20 @@ namespace Auth.Api.EndpointBuilders
                 await loginCommandHandler.SendAsync(loginCommand, cancellationToken))
                     .Produces<string>()
                     .WithName("Login")
-                    .AllowAnonymous()
-                    .WithOpenApi();
+                    .AllowAnonymous();
 
             endpointRouteBuilder.MapGet("/user",
                 async ([FromServices] IHttpRequestHandler<UserInfoDto, EmptyRequest> getUserInfoCommandHandler, CancellationToken cancellationToken) =>
                 await getUserInfoCommandHandler.SendAsync(EmptyRequest.Instance, cancellationToken))
                     .Produces<IEnumerable<UserInfoDto>>()
-                    .WithName("UserInfo")
-                    .WithOpenApi();
+                    .WithName("UserInfo");
 
             endpointRouteBuilder.MapPost("/register",
                 async (RegisterCommand loginCommand, [FromServices] IHttpRequestHandler<bool, RegisterCommand> registerCommandHandler, CancellationToken cancellationToken) =>
                 await registerCommandHandler.SendAsync(loginCommand, cancellationToken))
                     .Produces<bool>()
                     .WithName("Register")
-                    .AllowAnonymous()
-                    .WithOpenApi();
+                    .AllowAnonymous();
 
             return endpointRouteBuilder;
         }
